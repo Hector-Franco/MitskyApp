4acba17207dffaf68168ce34eb79d53d96564a80
Hector Franco <hector-franco@upc.edu.co>
2019-11-13 12:42:19 -0500

    Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {
+    component: HomeComponent,
+    path: ''
+  },
+  {
+    component: LoginComponent,
+    path: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import { MatGridListModule} from '@angular/material';
+
+// COMPONENTS
+import { RegisterComponent } from './components/register/register.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClubsComponent } from './components/clubs/clubs.component';
+import { HomeComponent } from './components/home/home.component';
+import { EventsComponent } from './components/events/events.component';
+import { BillingsComponent } from './components/billings/billings.component';
+import { BookingsComponent } from './components/bookings/bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    ClubsComponent,
+    HomeComponent,
+    EventsComponent,
+    BillingsComponent,
+    BookingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
